c3b0165d4699d191c158be54e3431ddb15bf4669
5t3v3V1 <steven.nguyen.ackworth@gmail.com>
2026-06-19 00:45:59 +0100

    Board and grid formatting fixed

--- frontend/script.js
+++ frontend/script.js
@@ -7,18 +7,50 @@ const bfs_type = document.getElementById("bfs_type");
 const bfs_nodes = document.getElementById("bfs_nodes");
 const bfs_grid = document.getElementById("bfs_grid");
 const bfs_steps = document.getElementById("bfs_steps")
+const bfs_time = document.getElementById("bfs_time");
 const dfs_type = document.getElementById("dfs_type");
 const dfs_nodes = document.getElementById("dfs_nodes");
 const dfs_grid = document.getElementById("dfs_grid");
 const dfs_steps = document.getElementById("dfs_steps")
+const dfs_time = document.getElementById("dfs_time");
 const dijkstra_type = document.getElementById("dijkstra_type");
 const dijkstra_nodes = document.getElementById("dijkstra_nodes");
 const dijkstra_grid = document.getElementById("dijkstra_grid");
 const dijkstra_steps = document.getElementById("dijkstra_steps")
+const dijkstra_time = document.getElementById("dijkstra_time");
 const astar_type = document.getElementById("astar_type");
 const astar_nodes = document.getElementById("astar_nodes");
 const astar_grid = document.getElementById("astar_grid");
 const astar_steps = document.getElementById("astar_steps")
+const astar_time = document.getElementById("astar_time");
+const winner = document.getElementById("winner");
+const board_time = document.getElementById("board_time");
+
+
+
+function format_board(board) {
+    format = "";
+
+    for (let row = 0; row < board.length; row ++) {
+        for (let cell = 0;  cell < board[row].split("").length; cell ++) {
+            format += `<div>${board[row].split("")[cell]}</div>`
+        };
+    };
+
+    return format;
+};
+
+function format_grid(grid) {
+    format = "";
+
+    for (let row = 0; row < grid.length; row ++) {
+        for (let cell = 0; cell < grid[row].split("").length; cell ++) {
+            format += `<div>${grid[row].split("")[cell]}</div>`
+        };
+    };
+
+    return format
+};
 
 generate_solve_board_button.addEventListener("click", async () => {
     const response = await fetch("http://127.0.0.1:8000/generate_solve_board");
@@ -26,8 +58,9 @@ generate_solve_board_button.addEventListener("click", async () => {
     const data = await response.json();
 
     console.log(data);
-    generated_board.innerText = data.generated_board.join("\n");
-    solved_board.innerText = data.solved_board.join("\n");
+    generated_board.innerHTML = format_board(data.generated_board);
+    solved_board.innerHTML = format_board(data.solved_board);
+    board_time.innerText = `Solve Time: ${data.time_ms.toFixed(2)}ms`
 })
 
 generate_solve_grid_button.addEventListener("click", async () => {
@@ -36,21 +69,36 @@ generate_solve_grid_button.addEventListener("click", async () => {
     const data = await response.json();
 
     console.log(data);
-    generated_grid.innerText = data.generated_grid.join("\n");
-    bfs_type.innerText = "BFS";
+    generated_grid.innerHTML = format_grid(data.generated_grid);
+    bfs_type.innerHTML = "<h3>BFS</h3>";
     bfs_nodes.innerText = `Nodes Visited: ${data.bfs.bfs_nodes}`;
-    bfs_grid.innerText = `Solved Grid: \n${data.bfs.solved_bfs_grid.join("\n")}`;
+    bfs_grid.innerHTML = `Solved Grid: ${format_grid(data.bfs.solved_bfs_grid)}`;
     bfs_steps.innerText = `Steps: ${data.bfs.bfs_steps.length}`;
-    dfs_type.innerText = "DFS";
+    bfs_time.innerText = `Solve Time: ${data.bfs.time_ms.toFixed(2)}ms`;
+    dfs_type.innerHTML = "<h3>DFS</h3>";
     dfs_nodes.innerText = `Nodes Visited: ${data.dfs.dfs_nodes}`;
-    dfs_grid.innerText = `Solved Grid: \n${data.dfs.solved_dfs_grid.join("\n")}`;
+    dfs_grid.innerHTML = `Solved Grid: ${format_grid(data.dfs.solved_dfs_grid)}`;
     dfs_steps.innerText = `Steps: ${data.dfs.dfs_steps.length}`;
-    dijkstra_type.innerText = "Dijkstra";
+    dfs_time.innerText = `Solve Time: ${data.dfs.time_ms.toFixed(2)}ms`;
+    dijkstra_type.innerHTML = "<h3>Dijkstra</h3>";
     dijkstra_nodes.innerText = `Nodes Visited: ${data.dijkstra.dijkstra_nodes}`;
-    dijkstra_grid.innerText = `Solved Grid: \n${data.dijkstra.solved_dijkstra_grid.join("\n")}`;
+    dijkstra_grid.innerHTML = `Solved Grid: ${format_grid(data.dijkstra.solved_dijkstra_grid)}`;
     dijkstra_steps.innerText = `Steps: ${data.dijkstra.dijkstra_steps.length}`;
-    astar_type.innerText = "A*";
+    dijkstra_time.innerText = `Solve Time: ${data.dijkstra.time_ms.toFixed(2)}ms`;
+    astar_type.innerHTML = "<h3>A*</h3>";
     astar_nodes.innerText = `Nodes Visited: ${data.astar.astar_nodes}`;
-    astar_grid.innerText = `Solved Grid: \n${data.astar.solved_astar_grid.join("\n")}`;
+    astar_grid.innerText = `Solved Grid: ${format_grid(data.astar.solved_astar_grid)}`;
     astar_steps.innerText = `Steps: ${data.astar.astar_steps.length}`;
+    astar_time.innerText = `Solve Time: ${data.astar.time_ms.toFixed(2)}ms`;
+
+    const results = [
+        {name: "BFS", time: data.bfs.time_ms},
+        {name: "DFS", time: data.dfs.time_ms},
+        {name: "Dijkstra", time: data.dijkstra.time_ms},
+        {name: "A*", time: data.astar.time_ms}
+    ];
+
+    results.sort((a,b) => a.time - b.time);
+
+    winner.innerText = `Best Algorithm: ${results[0].name}`;
 })
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from solver import solver
 import random
+import copy
+import time
 from validator import is_valid
 from classes import Board, SudokuRequest, Grid, Grid_Node, GridRequest
 from generator import board_generator, grid_generator
 from pathfinding_algorithm import bfs, dfs, dijkstra, astar
 from fastapi.middleware.cors import CORSMiddleware
-import copy
 valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -84,8 +85,12 @@ def generate_solve_board():
         board.nodes[position].number = 0
 
     generated_board = copy.deepcopy(board)
+
+    start = time.perf_counter()
     
     solved = solver(board)
+
+    end = time.perf_counter()
 
     if not solved:
         return {
@@ -98,6 +103,7 @@ def generate_solve_board():
         "solved": solved,
         "generated_board": generated_board.to_list(),
         "solved_board": board.to_list(),
+        "time_ms": (end - start) * 1000
     }
 
 @app.get("/generate_solve_grid")
@@ -112,18 +118,28 @@ def generate_solve_grid():
     dijkstra_grid = Grid(grid)
     astar_grid = Grid(grid)
     
-
+    bfs_start = time.perf_counter()
     bfs_nodes, solved_bfs_grid, bfs_steps = bfs(bfs_grid)
+    bfs_end = time.perf_counter()
+
+    dfs_start = time.perf_counter()
     dfs_nodes, solved_dfs_grid, dfs_steps = dfs(dfs_grid)
+    dfs_end = time.perf_counter()
+
+    dijkstra_start = time.perf_counter()
     dijkstra_nodes, solved_dijkstra_grid, dijkstra_steps = dijkstra(dijkstra_grid)
+    dijkstra_end = time.perf_counter()
+
+    astar_start = time.perf_counter()
     astar_nodes, solved_astar_grid, astar_steps = astar(astar_grid)
+    astar_end = time.perf_counter()
 
     return {
         "generated_grid": generated_grid.to_list(),
-        "bfs": {"bfs_nodes": bfs_nodes, "solved_bfs_grid": solved_bfs_grid.to_list(), "bfs_steps": bfs_steps},
-        "dfs": {"dfs_nodes": dfs_nodes, "solved_dfs_grid": solved_dfs_grid.to_list(), "dfs_steps": dfs_steps},
-        "dijkstra": {"dijkstra_nodes": dijkstra_nodes, "solved_dijkstra_grid": solved_dijkstra_grid.to_list(), "dijkstra_steps": dijkstra_steps},
-        "astar": {"astar_nodes": astar_nodes, "solved_astar_grid": solved_astar_grid.to_list(), "astar_steps": astar_steps},
+        "bfs": {"bfs_nodes": bfs_nodes, "solved_bfs_grid": solved_bfs_grid.to_list(), "bfs_steps": bfs_steps, "time_ms": (bfs_end - bfs_start) * 1000},
+        "dfs": {"dfs_nodes": dfs_nodes, "solved_dfs_grid": solved_dfs_grid.to_list(), "dfs_steps": dfs_steps, "time_ms": (dfs_end - dfs_start) * 1000},
+        "dijkstra": {"dijkstra_nodes": dijkstra_nodes, "solved_dijkstra_grid": solved_dijkstra_grid.to_list(), "dijkstra_steps": dijkstra_steps, "time_ms": (dijkstra_end - dijkstra_start) * 1000},
+        "astar": {"astar_nodes": astar_nodes, "solved_astar_grid": solved_astar_grid.to_list(), "astar_steps": astar_steps, "time_ms": (astar_end - astar_start) * 1000},
     }
 
 @app.post("/solve_grid")
